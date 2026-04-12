dac376c66be8179428b0f6fc46b89545570444fb
Jannael Orlando <orlandojannael@gmail.com>
2026-04-12 10:30:38 -0600

    test: testing the domain for add command

---
+++ src/test/add/domain/change.test.ts
@@ -0,0 +1,55 @@
+import { describe, expect, it } from 'vitest'
+import { Change } from '../../../add/domain/change'
+
+describe('Change', () => {
+	it('Create a change instance', () => {
+		const change = new Change({
+			value: 'test',
+			status: 'A',
+			displayPath: 'test',
+		})
+		expect(change.value).toBe('test')
+		expect(change.status).toBe('A')
+		expect(change.displayPath).toBe('test')
+	})
+
+	it('Get label', () => {
+		const change = new Change({
+			value: 'test',
+			status: 'A',
+			displayPath: 'test',
+		})
+
+		expect(change.label).toBe('\x1b[32mnew file:\x1b[0m test')
+	})
+
+	it('Is sensitive', () => {
+		const change = new Change({
+			value: '.env',
+			status: 'A',
+			displayPath: '.env',
+		})
+
+		expect(change.isSensitive()).toBe(true)
+	})
+
+	it('Get warning', () => {
+		const change = new Change({
+			value: '.env',
+			status: 'A',
+			displayPath: '.env',
+		})
+
+		expect(change.getWarning()).toBe(
+			'\x1b[33m .env file hidden\x1b[0m (Add to .gitignore to avoid leaks)',
+		)
+	})
+
+	it('Get changes from porcelain', () => {
+		const changes = Change.fromPorcelain([' M bun.lock'])
+		expect(changes).toHaveLength(1)
+		expect(changes[0]?.value).toBe('bun.lock')
+		expect(changes[0]?.status).toBe(' M')
+		expect(changes[0]?.displayPath).toBe('bun.lock')
+	})
+})
