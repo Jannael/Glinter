2b0cade5d1273cbff51f0582685a2d0240b9ff6d
Jannael Orlando <orlandojannael@gmail.com>
2026-04-22 08:40:36 -0600

    feat: changing dynamic date for total downloads api, for static date in 2100, because in cloudfare workers the new Date(), it is 1970?????

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -3,7 +3,7 @@
 export const REPO_API = 'https://api.github.com/repos/jannael/glinter'
 export const WEEKLY_DOWNLOADS_API =
 	'https://api.npmjs.org/downloads/point/last-week/@jannael/glinter'
-export const TOTAL_DOWNLOADS_API = `https://api.npmjs.org/downloads/point/2015-01-01:${new Date().toISOString().slice(0, 10)}/@jannael/glinter`
+export const TOTAL_DOWNLOADS_API = `https://api.npmjs.org/downloads/point/2026-03-01:2100-01-01/@jannael/glinter`
 
 export const STARS_CACHE_TTL_SECONDS = 60 * 5 * 2
 export const STATS_CACHE_TTL_SECONDS = 60 * 60 * 12
